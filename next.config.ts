37cc7c0d27b3232b3d77538d3b1cffb8363c5ea9
andiaiyy <118333971+andiaiyy@users.noreply.github.com>
2025-06-20 04:52:16 +0800

    next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -24,7 +24,7 @@ const nextConfig: NextConfig = {
     return [
       {
         source: "/api/:path*",
-        destination: "http://localhost:8000/api/:path*", // Proxy to Laravel backend
+        destination: "https://www.sarynthelabel.my.id/api/:path*", // Proxy to Laravel backend
       },
     ];
   },
